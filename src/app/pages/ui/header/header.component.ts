2f78cbae06ac3780fb9c09f8de51bbf5f17fc9a6
James Muñoz Coronel <j2mc66@hotmail.com>
2018-12-21 00:07:36 -0500

    configuracion sidebar

--- src/app/pages/ui/header/header.component.ts
+++ src/app/pages/ui/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @Input('active')
+  active: boolean;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggle(active){
+    console.log(active);
+    this.active = active;
   }
 
 }
--- src/app/pages/ui/layout/layout.component.ts
+++ src/app/pages/ui/layout/layout.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LayoutComponent implements OnInit {
 
+  active: boolean = true;
+
   constructor() { }
 
   ngOnInit() {
   }
-
+  
 }
